acb0ca20dec2dcf58cd1ab0bce14d6f8f9e6920c
Admin <fritzebner@gmail.com>
2016-05-03 09:27:16 -0400

    Initial Commit

---
+++ testwebviewmanifest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  testwebviewmanifest
+//
+//  Created by Admin on 5/3/16.
+//  Copyright © 2016 fritzco. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ testwebviewmanifest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  testwebviewmanifest
+//
+//  Created by Admin on 5/3/16.
+//  Copyright © 2016 fritzco. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
